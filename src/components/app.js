5dd7b690437d12b1f7e68a5e655020c545fa5851
Felicia Wold <feliciawold@gmail.com>
2019-02-20 20:21:05 -0700

    counter component

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
--- src/components/pages/counter.js
+++ src/components/pages/counter.js
@@ -1,9 +1,35 @@
 import React from "react";
 
-export default function() {
-    return (
-        <div>
-            Press our buttons
-        </div>
-    );
+
+export default class CountMe extends React.Component {
+    constructor() {
+    super()
+    this.state = {count: 0}
+    this.handleCountMe = this.handleCountMe.bind(this)
+
+    }
+
+    handleCountMe(num) {
+        this.setState({
+            count: this.state.count + num
+        })
+    }
+
+
+    render() {
+        return(
+            <div>
+                <h6>Press our buttons</h6>
+                <div>
+                    <button onClick={() => this.handleCountMe(1)}>Click Me to Count!</button>
+                    <button onClick={() => this.handleCountMe(-1)}>Click Me to Decrement</button>
+                </div>
+                <div>Shoobydoo the count is: { this.state.count }</div>
+            </div>
+        )
+    }
 }
+
+
+
+
